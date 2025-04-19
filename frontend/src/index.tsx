9d880ffc74f913b06943169b2e74b9de21f729d9
jeesup0103 <jeesup0103@gmail.com>
2025-04-19 16:05:52 +0900

    Remove unused files

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 if (!clientId) {
@@ -20,8 +19,3 @@ root.render(
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
