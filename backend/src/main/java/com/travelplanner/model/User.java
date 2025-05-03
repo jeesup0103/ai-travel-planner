3f0c69668ce9a143e8b0df77371836c9a50e24e6
jeesup0103 <jeesup0103@gmail.com>
2025-05-03 23:07:23 +0900

    Remove JwtConfig and make new User model

---
+++ backend/src/main/java/com/travelplanner/model/User.java
@@ -0,0 +1,44 @@
+package com.travelplanner.model;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String name;
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    protected User() {}
+
+    public User(String email, String name, String picture, Role role) {
+        this.email = email;
+        this.name = name;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    @ElementCollection
+    @CollectionTable(name = "user_travel_preferences", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "preference")
+    private List<String> travelPreferences = new ArrayList<>();
+
+}
