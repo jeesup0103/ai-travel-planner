fe7d1e35b2a9f0f736119b72feeb2e4fdd94a424
jeesup0103 <jeesup0103@gmail.com>
2025-05-10 20:34:10 +0900

    Add role in frontend user interface

--- frontend/src/types/user.ts
+++ frontend/src/types/user.ts
@@ -3,6 +3,7 @@ export interface User {
   email: string;
   name: string;
   picture?: string;
+  role: string;
   preferences?: {
     favoriteLocations?: string[];
     travelPreferences?: string[];
