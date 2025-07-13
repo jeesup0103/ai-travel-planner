22bcdc09dc50808d4b14ac662b73b5c41ce3cac7
jeesup0103 <jeesup0103@gmail.com>
2025-07-13 15:53:04 +0900

    Fix aiRecommendation json property

--- backend/src/main/java/com/travelplanner/dto/ChatResponse.java
+++ backend/src/main/java/com/travelplanner/dto/ChatResponse.java
@@ -1,10 +1,12 @@
 package com.travelplanner.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ChatResponse {
+    @JsonProperty("recommendation")
     private AiRecommendation aiRecommendation;
 }
